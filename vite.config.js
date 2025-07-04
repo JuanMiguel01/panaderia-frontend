071356ee89e33133202506c4d1b00cab59d1c2ce
Juan Miguel <juanmiguelperez524@gmail.com>
2025-07-04 19:12:24 -0400

    Preparar para despliegue en producción

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          socket: ['socket.io-client'],
+          http: ['axios']
+        }
+      }
+    }
+  },
+  preview: {
+    port: 5173,
+    host: true
+  }
 })
